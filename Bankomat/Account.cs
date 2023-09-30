16f8663b3ba2650530b7dd5a03648e202333d712
Asuka Hanada <hanadasuka@gmail.com>
2023-09-30 21:51:51 +0200

    Made class and added two methods for login.

--- Bankomat/Account.cs
+++ Bankomat/Account.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +9,41 @@ namespace Bankomat
 {
     public class Account
     {
-        public int UserMatch(string userName)
+        public int NameCheck()
         {
+            string[] userNames = { "AYA", "AKI", "TOBIAS", "MUSSEPIG", "DONALD" };
             int index = 0;
-          return index;
+            Console.Write("Insert your name: ");
+            string inputName = Console.ReadLine();
+            for (int i = 0; i < userNames.Length; i++)
+            {
+                if (userNames[i] == inputName.ToUpper())
+                {
+                    index = i;  
+                    return index;
+                }
+            }
+            return -1;
         }
-        public bool IsMatch()
+        public bool PassCheck(int index)
         {
-            Console.WriteLine();
-            return true;
+            int[] passWords = { 1234, 1221, 2345, 3456, 4567 };
+            
+            for (int i = 0; i < 3; i++) 
+            {
+                Console.Write("Insert your password: ");
+                int inputPass = Convert.ToInt32(Console.ReadLine());
+                if (passWords[index] == inputPass)
+                {
+                    Console.WriteLine("Password is not correct.");
+                    return true;
+                }
+                else
+                    Console.WriteLine("Password is not correct.");
+            }
+            Console.WriteLine("You tried 3 times. Please try again after 3 minutes.\n Either username or password is not correct.");
+            return false;
+            
         }
 
         public static void Menu()
@@ -27,6 +54,7 @@ namespace Bankomat
             string menu4 = "4. Logga ut";
 
             Console.WriteLine(menu1 + "\n" + menu2 + "\n" + menu3 + "\n" + menu4);
+            
 
         }
     }
--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -4,31 +4,18 @@
     {
         static void Main(string[] args)
         {
-            string[] userNames = {"AYA", "AKI", "TOBIAS", "MUSSEPIG", "DONALD" };
-            int[] passWords = {1234, 1221, 2345, 3456, 4567 };
             Console.WriteLine("Välkommen till banken!");
-            Console.Write("Insert your name: ");
-            string inputName = Console.ReadLine();
-            int index = 0;
-            for(int i = 0; i < userNames.Length; i++)
-            {
-                if (userNames[i] == inputName.ToUpper()) 
-                { 
-                    index = i;
-                    Console.Write("Insert your password: ");
-                    int inputPass = Convert.ToInt32(Console.ReadLine());
-                    if (passWords[index] != inputPass)
-                    {
-                        Console.WriteLine("Password is not correct.");
-                    }
-                    else
-                    {
-                        Account.Menu();
-                    }
-                }
+            Account user = new Account();
+            int index = user.NameCheck();
+            bool approved = user.PassCheck(index) && index != -1;
 
+            while (approved) 
+            { 
+                Account.Menu();
+                Console.WriteLine("What can I help you today?");
+                int menuChoice = Convert.ToInt32(Console.ReadLine()); ;
             }
-            
+
         }
     }
 }
