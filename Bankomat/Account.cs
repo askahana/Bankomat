1f376c96645b8ddd716c045826bed82355eb459f
Asuka Hanada <hanadasuka@gmail.com>
2023-09-30 23:45:15 +0200

    Added switch statement so that the user can make choce. Added more methods in the class, Account. Methods to check the balance, to remove monety from the account.

--- Bankomat/Account.cs
+++ Bankomat/Account.cs
@@ -28,34 +28,46 @@ namespace Bankomat
         public bool PassCheck(int index)
         {
             int[] passWords = { 1234, 1221, 2345, 3456, 4567 };
-            
             for (int i = 0; i < 3; i++) 
             {
                 Console.Write("Insert your password: ");
                 int inputPass = Convert.ToInt32(Console.ReadLine());
                 if (passWords[index] == inputPass)
                 {
-                    Console.WriteLine("Password is not correct.");
+                    Console.WriteLine("Login succeded");
+                    Console.ReadKey();
                     return true;
                 }
                 else
                     Console.WriteLine("Password is not correct.");
             }
             Console.WriteLine("You tried 3 times. Please try again after 3 minutes.\n Either username or password is not correct.");
-            return false;
-            
+            return false;   
         }
-
         public static void Menu()
         {
+            Console.Clear();
+            Console.WriteLine("What can I help you today?");
             string menu1 = "1. Se dina konton och saldo";
             string menu2 = "2. Överföring mellan konton";
             string menu3 = "3. Ta ut pengar";
             string menu4 = "4. Logga ut";
-
             Console.WriteLine(menu1 + "\n" + menu2 + "\n" + menu3 + "\n" + menu4);
-            
-
         }
+        public static void CheckBalance(List<double>a, int index)
+        {
+            Console.WriteLine(a[index]);
+            Console.ReadKey();
+        }
+        public static void Move(List<double>a, int index, double money, int x)
+        {
+            a[index] -= money;
+            a[x] += money;
+        }
+        public static void Remove(List<double> a, int index, double money)
+        {
+            a[index] -= money;
+        }
+
     }
 }
--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -8,12 +8,35 @@
             Account user = new Account();
             int index = user.NameCheck();
             bool approved = user.PassCheck(index) && index != -1;
-
+            List<double> balance = new List<double>();
+            balance.Add(123.35);
+            balance.Add(0);
+            balance.Add(234.45);
+            balance.Add(2300);
+            balance.Add(3456);
             while (approved) 
-            { 
+            {
+                
                 Account.Menu();
-                Console.WriteLine("What can I help you today?");
-                int menuChoice = Convert.ToInt32(Console.ReadLine()); ;
+                int menuChoice = Convert.ToInt32(Console.ReadLine());
+                switch (menuChoice)
+                {
+                    case 1:
+                        Account.CheckBalance(balance, index);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        Account.CheckBalance(balance, index);
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Thanks for using.");
+                        approved = false;
+                        break;
+                    default:
+                        break;
+                }
             }
 
         }
