fbe9aec10b8e1c4d743a76dcd7500da7036d51a7
Asuka Hanada <hanadasuka@gmail.com>
2023-09-30 21:26:58 +0200

    Add project files.

---
+++ Bankomat/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    public class Account
+    {
+        public int UserMatch(string userName)
+        {
+            int index = 0;
+          return index;
+        }
+        public bool IsMatch()
+        {
+            Console.WriteLine();
+            return true;
+        }
+
+        public static void Menu()
+        {
+            string menu1 = "1. Se dina konton och saldo";
+            string menu2 = "2. Överföring mellan konton";
+            string menu3 = "3. Ta ut pengar";
+            string menu4 = "4. Logga ut";
+
+            Console.WriteLine(menu1 + "\n" + menu2 + "\n" + menu3 + "\n" + menu4);
+
+        }
+    }
+}
---
+++ Bankomat/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Bankomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNames = {"AYA", "AKI", "TOBIAS", "MUSSEPIG", "DONALD" };
+            int[] passWords = {1234, 1221, 2345, 3456, 4567 };
+            Console.WriteLine("Välkommen till banken!");
+            Console.Write("Insert your name: ");
+            string inputName = Console.ReadLine();
+            int index = 0;
+            for(int i = 0; i < userNames.Length; i++)
+            {
+                if (userNames[i] == inputName.ToUpper()) 
+                { 
+                    index = i;
+                    Console.Write("Insert your password: ");
+                    int inputPass = Convert.ToInt32(Console.ReadLine());
+                    if (passWords[index] != inputPass)
+                    {
+                        Console.WriteLine("Password is not correct.");
+                    }
+                    else
+                    {
+                        Account.Menu();
+                    }
+                }
+
+            }
+            
+        }
+    }
+}
