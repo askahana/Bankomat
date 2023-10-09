91a38378609985fdaa824087d148e9dc83766b5d
Asuka Hanada <hanadasuka@gmail.com>
2023-10-09 23:57:01 +0200

    Added comments and one more bool-statement to avoid repeated codes.

--- Bankomat/Account.cs
+++ Bankomat/Account.cs
@@ -10,59 +10,59 @@ namespace Bankomat
 {
     public class Account
     {
-        private string[] UserNames = { "AYA", "AKI", "TOBIAS", "MUSSEPIG", "OLLE" };
-        private int[] PassWords = { 1103, 1221, 2345, 3456, 0901 };
+        private List<string> UserNames = new List<string> { "AYA", "AKI", "TOBIAS", "MUSSEPIG", "OLLE" };
+        private List<int> PassWords = new List<int> { 1103, 1221, 2345, 3456, 0901 };
         private string[] AccountNames = { "Lönekonto", "Sparkonto", "Aktiekonto", "Betalkonto", "Privatkonto" };
-        private decimal[][] Balance = new decimal[5][] 
+        private decimal[][] Balance = new decimal[5][]
         {
-        new decimal[] { 12340, 1234 },
-        new decimal[] { 234, 2345, 234 },
+        new decimal[] { 12340, 1234, 23456, 45677 },
+        new decimal[] { 234, 2345, 234, 34567 },
         new decimal[] { 123, 234 },
         new decimal[] { 45999, 56778, 3456 },
-        new decimal[] { 123, 234 }
+        new decimal[] { 123, 234, 34567 }
         };
-        public int CheckName()
+        public int CheckNameAndIndex()                  
         {
-            int index = 0;
+            int index = 0;                                  // För att kunna veta vilket index-nummer användare har.
             Console.Write("Ange ditt namn: ");
-           
-                string inputName = Console.ReadLine();
-                for (int i = 0; i < UserNames.Length; i++)
+            string inputName = Console.ReadLine();          // Användare matar in användarnamn.
+            for (int i = 0; i < UserNames.Count; i++)
+            {
+                if (UserNames[i] == inputName.ToUpper())
                 {
-                    if (UserNames[i] == inputName.ToUpper())
-                    {
-                        index = i;
-                        return index;
-                    }
+                    index = i;                              // Om användarnamn finns i Array, returneras index.
+                    return index;
                 }
-            return -1;
-        }
-        public bool CheckPass(int index)
+            }
+            return -1;                                      // Om användarens inmatning och namnen i Array är inte samma,
+        }                                                   // returneras -1.
+        public bool CheckPass(int index)        // Kontrollera pin-kod.
         {
-            if (index == -1)
+            if (index == -1)                    // Om användarnamn inte stämmer, får användaren inte skriva pin-koden.
             {
                 Console.WriteLine("Fel användarnamn. Försök igen.");
                 Console.ReadKey();
                 return false;
             }
-                for (int i = 0; i < 3; i++)
-                {
+            for (int i = 0; i < 3; i++)     // Användaren får skriva pinkod max 3 gånger.
+            {
                 Console.Write("Ange pin-kod: ");
-                try { 
-                    int inputPass = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    int inputPass = Convert.ToInt32(Console.ReadLine());        // Användare matar in pinkod.
                     if (PassWords[index] == inputPass)
                         return true;
                     else
                         Console.WriteLine("Felaktig pin-kod. Vänligen försök igen.");
-                    }
-                    catch (Exception e)
-                    {
-                    Console.WriteLine("Felaktig pin-kod! Ange siffror.");
-                    }
                 }
-            Console.WriteLine("You tried 3 times. Please try again later after 3 minutes.");
-            Thread.Sleep(1000*60*3);
-            return false;
+                catch (Exception e)
+                {
+                    Console.WriteLine("Felaktig pin-kod! Ange siffror.");
+                }
+            }
+            Console.WriteLine("Du har försökt 3 gånger. Vänligen vänta 3 minuter.");
+            Thread.Sleep(1000 * 60 * 3);         // Om användaren skriver in fel pinkod tre gånger, måste anävndaren
+            return false;                        // vänta 3 minuter.
         }
         public static void Menu()
         {
@@ -73,20 +73,24 @@ namespace Bankomat
             string menu3 = "3. Ta ut pengar";
             string menu4 = "4. Sätta in pengar";
             string menu5 = "5. Logga ut";
-            Console.WriteLine("\n\t"+menu1 + "\n\t" + menu2 + "\n\t" + menu3 + "\n\t" + menu4 + "\n\t" + menu5);
+            Console.WriteLine("\n\t" + menu1 + "\n\t" + menu2 + "\n\t" + menu3 + "\n\t" + menu4 + "\n\t" + menu5);
         }
-        public void CheckBalance(int index)
+        public void CheckBalance(int index)         // Här kan användare se saldo.
         {
             Console.Clear();
-            Console.WriteLine("--------Dina konto hos oss--------");
-            for (int i = 0; i < Balance[index].Length; i++) {
-                Console.WriteLine( $"Saldo på {AccountNames[i]} : {Balance[index][i]} kr");
-            }
+            Console.WriteLine("--------Saldo--------");
+            ShowAccount(index);
             Console.WriteLine("\nKlicka enter för att komma till huvudmenyn");
             Console.ReadKey();
         }
-        public bool XisNotValid(int x, int index)
-        {
+        public void ShowAccount(int index)      // Den här visar Kontonamn och saldo. 
+        {                                       // Skaffade den här för att slippa upprepade koder.
+            for (int i = 0; i < Balance[index].Length; i++)
+                Console.WriteLine($"{i+1}. {AccountNames[i]} : {Balance[index][i]} kr");
+            Console.WriteLine();
+        }
+        public bool XisNotValid(int x, int index)           // Den här används vid överföring etc. Om kontonnummer som användaren
+        {                                                   // matar in inte finns på index-nummer, är det true.
             if (x <= 0 || x > Balance[index].Length)
             {
                 Console.WriteLine($"Ange ett tal mellan 1 till {Balance[index].Length}. \nKlicka enter för att komma till huvudmenyn.");
@@ -95,76 +99,75 @@ namespace Bankomat
             }
             return false;
         }
-        public void Transfer (int index)
+        public bool BalanceIsTooHigh(int index, decimal money, int x)
+        {
+            if (Balance[index][x - 1] < money || money < 0)
+            {    // Om användaren vill ta ut mer pengar än det som finns i kontot, eller om det är ett negativ tal.
+                Console.WriteLine("\nBeloppet är för stort eller negativ tal. Vänligen försök igen.");
+                Console.ReadKey();
+                return true ;
+            }
+            return false;
+        }
+        public void Transfer(int index)                    // Överföra pengar.
         {
             Console.Clear();
-            Console.WriteLine("--------Överföra pengar--------");
-            Console.WriteLine("Vilket konto vill du överföra pengar från?");
-            for (int i = 0; i < Balance[index].Length; i++)
-                Console.WriteLine((i+1) + ". " + AccountNames[i]+ ": " + Balance[index][i] + " kr");
-            int x = Convert.ToInt32(Console.ReadLine());
-            if (XisNotValid(x, index) == true)
+            Console.WriteLine("--------Överföra pengar--------\nVilket konto vill du överföra pengar från?");
+            ShowAccount(index);     // Visa konto
+            int x = Convert.ToInt32(Console.ReadLine()); // Här lagras konto-nummer.
+            if (XisNotValid(x, index) == true)      // Om x är mindre eller större än antal konto, kan inte gå vidare.
                 return;
             Console.WriteLine("Vilket konto vill du lägga pengar till?");
             int y = Convert.ToInt32(Console.ReadLine());
-            if (XisNotValid(y, index)==true)
+            if (XisNotValid(y, index) == true)
                 return;
             Console.Write("Summa: ");
             decimal money = Convert.ToDecimal(Console.ReadLine());
-            if (Balance[index][x - 1] < money) { 
-                Console.WriteLine("Beloppet är för stort. Vänligen försök igen.");
-                Console.ReadKey();
+            if (BalanceIsTooHigh(index, money, x))
                 return;
-            }
-            else 
-            { 
-                Balance[index][x-1] -= money;
-                Balance[index][y-1] += money;
+            else
+            {
+                Balance[index][x - 1] -= money;         // Ta bort pengar från x-1 konto.
+                Balance[index][y - 1] += money;         // Addera pengar till y-1 konto.
             }
             Console.Clear();
             Console.WriteLine("--------Nuvarande saldo--------");
-            Console.WriteLine($"\n{AccountNames[x-1]}: {Balance[index][x-1]} kr");
-            Console.WriteLine($"{AccountNames[y-1]}: {Balance[index][y-1]} kr");
-            Console.WriteLine("\nKlicka enter för att komma till huvudmenyn");
+            Console.WriteLine($"\n{AccountNames[x - 1]}: {Balance[index][x - 1]} kr" +
+                $"\n{AccountNames[y - 1]}: {Balance[index][y - 1]} kr" +
+                $"\nKlicka enter för att komma till huvudmenyn");
             Console.ReadKey();
         }
-        public void Withdraw(int index)
+        public void Withdraw(int index)             // Ta ut pengar.
         {
             Console.Clear();
-            Console.WriteLine("--------TA UT PENGAR--------");
-            Console.WriteLine("Vilket konto vill du ta ut pengar?");
-            for (int i = 0; i < Balance[index].Length; i++)
-                Console.WriteLine((i+1) + ". " + AccountNames[i]+ ":  "+Balance[index][i]+" kr");
-            int x = Convert.ToInt32(Console.ReadLine());
-            if (XisNotValid(x, index)==true)
+            Console.WriteLine("--------TA UT PENGAR--------\nVilket konto vill du ta ut pengar?");
+            ShowAccount(index);
+            int x = Convert.ToInt32(Console.ReadLine());    // Här lagras konto-nummer.
+            if (XisNotValid(x, index) == true)
                 return;
             Console.Write("Mata in summan: ");
             decimal money = Convert.ToDecimal(Console.ReadLine());
-            bool approved = CheckPass(index);
-            while (approved) {
-                if (Balance[index][x - 1] < money)
-                {
-                    Console.WriteLine("Beloppet är för stort. Vänligen försök igen.");
-                    Console.ReadKey();
+            bool approved = CheckPass(index);       // kontrollerar pinkod.
+            while (approved)
+            {                      // Om pinkod stämmer kan användaren ta ut pengar.
+                if (BalanceIsTooHigh(index, money, x))
                     return;
-                }
                 else
                 {
-                    Balance[index][x - 1] -= money;
+                    Balance[index][x - 1] -= money;     // Ta bort summan från x-1 konto.
                     Console.WriteLine($"\nNuvarande saldo: {AccountNames[x - 1]}: {Balance[index][x - 1]} kr");
                     approved = false;
-                }   
+                }
             }
             Console.WriteLine("\nKlicka enter för att komma till huvudmenyn");
-            Console.ReadKey();   
+            Console.ReadKey();
         }
-        public void Deposit(int index)
+        public void Deposit(int index)          // sätta in pengar.
         {
             Console.Clear();
             Console.WriteLine("--------SÄTTA IN PENGAR--------");
             Console.WriteLine("Vilket konto vill du sätta in pengar?");
-            for (int i = 0; i < Balance[index].Length; i++)
-                Console.WriteLine((i + 1) + ". " + AccountNames[i]);
+            ShowAccount(index);
             int x = Convert.ToInt32(Console.ReadLine());
             if (XisNotValid(x, index) == true)
                 return;
@@ -173,14 +176,14 @@ namespace Bankomat
             bool approved = CheckPass(index);
             while (approved)
             {
-                    Balance[index][x - 1] += money;
-                    Console.WriteLine($"\nNuvarande saldo: {AccountNames[x - 1]}: {Balance[index][x - 1]} kr");
-                    approved = false;
+                Balance[index][x - 1] += money;     // Addera summan till x-1 konto.
+                Console.WriteLine($"\nNuvarande saldo: {AccountNames[x - 1]}: {Balance[index][x - 1]} kr");
+                approved = false;
             }
             Console.WriteLine("\nKlicka enter för att komma till huvudmenyn");
             Console.ReadKey();
         }
-        public void Farewell()
+        public void Farewell()      // Hejdå meddelande.
         {
             Console.Clear();
             Console.WriteLine("Tack för besöket. Välkommen åter!\nDu kommer nu tas tillbaka till inloggningsskärmen.");
--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -5,48 +5,49 @@
         static void Main(string[] args)
         {
             bool isWorking = true;
-            while (isWorking) 
+            while (isWorking)
             {
                 Console.Clear();
                 Console.WriteLine("Välkommen till Banken!");
                 Account user = new Account();
-                int index = user.CheckName();
-                bool approved = user.CheckPass(index) && index != -1;
-            while (approved) 
-            {
-                Account.Menu();
-                    try { 
-                    int menuChoice = Convert.ToInt32(Console.ReadLine());
-                    switch (menuChoice)
+                int index = user.CheckNameAndIndex();              // Användarens index-nummer.
+                bool approved = user.CheckPass(index);    // Om pinkod stämmer.
+                while (approved)
+                {
+                    Account.Menu();
+                    try      // Om användaren matar in annat än siffra, går till catch.
                     {
-                        case 1:
-                            user.CheckBalance(index);
-                            break;
-                        case 2:
-                            user.Transfer(index);
-                            break;
-                        case 3:
-                            user.Withdraw(index);
-                            break;
-                        case 4:
-                            user.Deposit(index);
-                            break;
-                        case 5:
-                            user.Farewell();
-                            approved = false;
-                            break;
-                        default:
-                                Console.WriteLine("Ogiltigt val");
+                        int menuChoice = Convert.ToInt32(Console.ReadLine());
+                        switch (menuChoice)
+                        {
+                            case 1:
+                                user.CheckBalance(index);
+                                break;
+                            case 2:
+                                user.Transfer(index);
+                                break;
+                            case 3:
+                                user.Withdraw(index);
+                                break;
+                            case 4:
+                                user.Deposit(index);
+                                break;
+                            case 5:
+                                user.Farewell();
+                                approved = false;
+                                break;
+                            default:
+                                Console.WriteLine("Ange tal mellan 1 till 5.");
                                 Console.ReadKey();
-                            break;
-                    }
+                                break;
+                        }
                     }
                     catch (Exception)
                     {
                         Console.WriteLine("ERROR!!! Ange ett tal.");
                         Console.ReadKey();
                     }
-            }
+                }
             }
         }
     }
